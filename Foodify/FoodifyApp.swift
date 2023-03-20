e3474d5cd8c340cdb75292908e324c02588a1104
tedtya rady <tedtyarady@gmail.com>
2023-03-20 12:20:59 +1100

    Initial Commit

---
+++ Foodify/FoodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodifyApp.swift
+//  Foodify
+//
+//  Created by Tedtya rady on 20/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
